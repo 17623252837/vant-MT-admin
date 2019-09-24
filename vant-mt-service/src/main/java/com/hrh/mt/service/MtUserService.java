b9bad70c8207d7550443d64c6fa3c4feacf34363
1505523898@qq.com <H19990727769x>
2019-09-24 15:16:43 +0800

    加入OSS云存储

--- vant-mt-service/src/main/java/com/hrh/mt/service/MtUserService.java
+++ vant-mt-service/src/main/java/com/hrh/mt/service/MtUserService.java
@@ -2,5 +2,4 @@ package com.hrh.mt.service;
 
 public interface MtUserService {
 
-
 }
---
+++ vant-mt-service/src/main/java/com/hrh/mt/service/PhoneCodeService.java
@@ -0,0 +1,7 @@
+package com.hrh.mt.service;
+
+public interface PhoneCodeService {
+    //获取验证码
+
+    //提交验证码
+}
